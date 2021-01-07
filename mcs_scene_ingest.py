1f4ed3e571979b30a4a1d2a8309ea16955cb6ff3
Brian Pippin <brian.pippin@caci.com>
2021-01-07 13:24:47 -0500

    Re-enable ingest line, remove print

--- mcs_scene_ingest.py
+++ mcs_scene_ingest.py
@@ -315,9 +315,7 @@ def ingest_history_files(folder: str, eval_name: str, performer: str, scene_fold
             else:
                 ingest_history[replacementIndex] = history_item
 
-    print(len(ingest_history))
-
-    #ingest_to_mongo(HISTORY_INDEX, ingest_history)
+    ingest_to_mongo(HISTORY_INDEX, ingest_history)
 
 
 def main(argv) -> None:
