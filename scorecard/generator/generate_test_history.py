8ad32248a953b02616ce3121b56356629664dc02
clark dorman <clarkus_dormanus@hotmail.com>
2021-08-04 08:01:12 -0400

    WIP

--- scorecard/generator/generate_test_history.py
+++ scorecard/generator/generate_test_history.py
@@ -3,7 +3,8 @@
 #
 import json
 
-from machine_common_sense import SceneHistory, HistoryWriter
+from machine_common_sense import SceneHistory, HistoryWriter, StepMetadata
+from machine_common_sense.config_manager import SceneConfigurationSchema
 
 
 def create_revisitor():
@@ -11,24 +12,23 @@ def create_revisitor():
     Create a datafile that goes one way across floor, then goes the other way,
     so that it crosses.  Should verify that it revisits.
     """
-
-    history = {}
-    steps = []
-
-    for step_num in range(1, 100):
-        step_dict = {'step': step_num}
-        metadata = SceneHistory()
-        metadata.position = {'x': 0.5, 'y': 1.0, 'z': (0.5 * (step_num * 0.07))}
-        step_dict['output'] = metadata
-        steps.append(step_dict)
-    history['steps'] = steps
-
-    scene_config_data = { 'name': 'test'}
+    scene_config_data = SceneConfigurationSchema()
+    scene_config_data.name = "test"
+    scene_config_data.screenshot = False
     history_writer = HistoryWriter(scene_config_data)
+    for step_number in range(1, 100):
+        step_output = StepMetadata(
+            position = {'x': 0.5, 'y': 1.0, 'z': (0.5 * (step_number * 0.07))}
+        )
+        history_item = SceneHistory(
+            step=step_number,
+            output=step_output)
+        history_writer.add_step(history_item)
+
     history_writer.write_history_file(.3, .2)
 
-    with open("../revisitor.json", "a+") as revisitor_file:
-        revisitor_file.write(json.dumps(history, indent=4))
+    # with open("../testdata/revisitor.json", "a+") as revisitor_file:
+    #     revisitor_file.write(json.dumps(history, indent=4))
 
 
 if __name__ == "__main__":
