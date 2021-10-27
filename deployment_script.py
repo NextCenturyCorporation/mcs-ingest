8d23af61f96c706bf2f703f59857ccdbae7af805
Rachel Artiss <rachel.artiss@caci.com>
2021-10-26 21:17:11 -0400

    Add new category field for eval 2 specifically, new indexes on history collection (MCS-516).

--- deployment_script.py
+++ deployment_script.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-from scripts._0_4_6_update_eval_2_test_types import update_test_types
+from scripts._0_4_7_update_history_indexes import create_new_cat_eval2_and_indexes
 
 # We might want to move mongo user/pass to new file
 VERSION_COLLECTION = "mcs_version"
 
 # Change this version if running a new deploy script
 # Make sure the first two numbers match the current MCS API Release
-db_version = "0.4.6"
+db_version = "0.4.7"
 
 
 def check_version(mongoDB):
@@ -30,7 +30,7 @@ def main():
     if(check_version(mongoDB)):
         print("New db version, execute scripts")
         # Place scripts here to run
-        update_test_types(mongoDB)
+        create_new_cat_eval2_and_indexes(mongoDB)
 
         # Now update db version
         update_db_version(mongoDB)
@@ -43,7 +43,7 @@ def main():
     if(check_version(mongoDB)):
         print("New db version, execute scripts")
         # Place scripts here to run
-        update_test_types(mongoDB)
+        create_new_cat_eval2_and_indexes(mongoDB)
 
         # Now update db version
         update_db_version(mongoDB)
