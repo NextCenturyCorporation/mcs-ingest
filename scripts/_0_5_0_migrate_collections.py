3834f00b299664dad5b41f569e6678a1ec8e7e6a
Brian Pippin <brian.pippin@caci.com>
2022-01-19 08:18:25 -0500

    Added functions to improve scripts readability

--- scripts/_0_5_0_migrate_collections.py
+++ scripts/_0_5_0_migrate_collections.py
@@ -1,3 +1,32 @@
+def copy_indexes(mongoDB, old_collection, new_collection_name):
+    new_scenes_collection = mongoDB[new_collection_name]
+    scene_indexes = old_collection.index_information()
+    for key in scene_indexes:
+        index_tuple = scene_indexes[key]["key"][0]
+        new_scenes_collection.create_index([(index_tuple[0], int(index_tuple[1]))])
+
+    print("Copied all indexes to: " + str(new_collection_name))
+
+
+def create_new_collection(collection, eval_name, collection_name, eval_number):
+    pipeline = [
+        {
+            "$match": {"eval": eval_name}
+        }, {
+            "$out": collection_name
+        }
+    ]
+
+    results = collection.aggregate(pipeline)
+
+    print("Create new Eval " + str(eval_number) + " Scenes Collection.", results)
+
+
+def mapping_insert(mapping_collection, eval_name, collection_name):
+    mapping_collection.insert_one({"name": eval_name, "collection": collection_name})
+    print("Added new mapping for: " + str(eval_name))
+
+
 def migrate_collections(mongoDB):
     history_collection = mongoDB["mcs_history"]
     scenes_collection = mongoDB["mcs_scenes"]
@@ -8,57 +37,22 @@ def migrate_collections(mongoDB):
     evalNumbers = [2, 3, 3.5, 3.75, 4]
 
     for en in evalNumbers:
+        # Handle logic for copying Scenes
         eval_scenes_pretty_name = "Evaluation " + str(en) + " Scenes"
         eval_scenes_collection_name = "eval_" + (str(en)).replace(".", "_") + "_scenes"
 
-        scene_mapping.insert_one({"name": eval_scenes_pretty_name, "collection": eval_scenes_collection_name})
+        mapping_insert(scene_mapping, eval_scenes_pretty_name, eval_scenes_collection_name)
+        create_new_collection(scenes_collection, eval_scenes_pretty_name, eval_scenes_collection_name, en)
+        copy_indexes(mongoDB, scenes_collection, eval_scenes_collection_name)
 
-        scene_pipeline = [
-            {
-                "$match": {"eval": eval_scenes_pretty_name}
-            }, {
-                "$out": eval_scenes_collection_name
-            }
-        ]
-
-        scene_results = scenes_collection.aggregate(scene_pipeline)
-
-        print("Create new Eval " + str(en) + " Scenes Collection.", scene_results)
-
-        new_scenes_collection = mongoDB[eval_scenes_collection_name]
-        scene_indexes = scenes_collection.index_information()
-        for key in scene_indexes:
-            index_tuple = scene_indexes[key]["key"][0]
-            new_scenes_collection.create_index([(index_tuple[0], int(index_tuple[1]))])
-
-        print("Copied all indexes to new Scenes Collection.")
-
+        # Handle logic for copying History
         eval_history_pretty_name = "Evaluation " + str(en) + " Results"
         eval_history_collection_name = "eval_" + (str(en)).replace(".", "_") + "_results"
 
-        history_mapping.insert_one({"name": eval_history_pretty_name, "collection": eval_history_collection_name})
+        mapping_insert(history_mapping, eval_history_pretty_name, eval_history_collection_name)
+        create_new_collection(history_collection, eval_history_pretty_name, eval_history_collection_name, en)
+        copy_indexes(mongoDB, history_collection, eval_history_collection_name)
 
-        scene_pipeline = [
-            {
-                "$match": {"eval": eval_history_pretty_name}
-            }, {
-                "$out": eval_history_collection_name
-            }
-        ]
-
-        history_results = history_collection.aggregate(scene_pipeline)
-
-        print("Create new Eval " + str(en) + " Results Collection.", history_results)
-
-        new_results_collection = mongoDB[eval_history_collection_name]
-        history_indexes = history_collection.index_information()
-        for key in history_indexes:
-            index_tuple = history_indexes[key]["key"][0]
-            new_results_collection.create_index([(index_tuple[0], int(index_tuple[1]))])
-
-        print("Copied all indexes to new Results Collection.")
-
+    # Remove old collections after new collections set up
     history_collection.drop()
     scenes_collection.drop()
-
-    
