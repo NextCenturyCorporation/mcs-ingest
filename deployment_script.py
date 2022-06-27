181de0f68372cc0d52fa006c4cc6cc4025e42cce
Rachel Artiss <rachel.artiss@caci.com>
2022-06-26 20:57:37 -0400

    Update scorecard fields for eval 4 to match those in eval 5 (MCS-1349).

--- deployment_script.py
+++ deployment_script.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-from scripts._0_5_5_add_slice_to_history import add_slice_to_history_items
+from scripts._0_5_6_update_scorecard_fields import update_scorecard_fields
 
 # We might want to move mongo user/pass to new file
 VERSION_COLLECTION = "mcs_version"
 
 # Change this version if running a new deploy script
 # Make sure the first two numbers match the current MCS API Release
-db_version = "0.5.4"
+db_version = "0.5.6"
 
 
 def check_version(mongoDB):
@@ -30,7 +30,7 @@ def main():
     if(check_version(mongoDB)):
         print("New db version, execute scripts")
         # Place scripts here to run
-        add_slice_to_history_items(mongoDB)
+        update_scorecard_fields(mongoDB)
 
         # Now update db version
         update_db_version(mongoDB)
@@ -43,7 +43,7 @@ def main():
     if(check_version(mongoDB)):
         print("New db version, execute scripts")
         # Place scripts here to run
-        add_slice_to_history_items(mongoDB)
+        update_scorecard_fields(mongoDB)
 
         # Now update db version
         update_db_version(mongoDB)
---
+++ scripts/_0_5_6_update_scorecard_fields.py
@@ -0,0 +1,34 @@
+def update_scorecard_fields(mongoDB):
+    results_collection = mongoDB["eval_4_results"]
+
+    result = results_collection.update_many(
+    {
+        "score.scorecard.multiple_container_look": {"$exists": True}
+    }, {
+        "$rename": {'score.scorecard.multiple_container_look': 'score.scorecard.container_relook'}
+    }, False)
+
+    print("update_many performed on " + str(
+        result.matched_count) + " documents")
+
+    # need to rename temporarily first to avoid "The source and target 
+    # field for $rename must not be on the same path" error
+    result = results_collection.update_many(
+    {
+        "score.scorecard.repeat_failed": {"$exists": True}
+    }, {
+        "$rename": {'score.scorecard.repeat_failed': 'temp_repeat'}
+    }, False)
+
+    result = results_collection.update_many(
+    {
+        "temp_repeat": {"$exists": True}
+    }, {
+        "$rename": {'temp_repeat': 'score.scorecard.repeat_failed.total_repeat_failed'}
+    }, False)
+
+    print("update_many performed on " + str(
+        result.matched_count) + " documents")
+
+
+    print("Updated scorecard fields from Eval 4.")
