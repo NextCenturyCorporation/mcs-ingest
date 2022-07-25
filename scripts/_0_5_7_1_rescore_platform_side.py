5dfbaa4f1b804c9053a95dfac4f859204bc70f98
Brian Pippin <brian.pippin@caci.com>
2022-07-25 09:57:01 -0400

    Update scripts to pull history file from S3 and use that to score platform side

--- scripts/_0_5_7_1_rescore_platform_side.py
+++ scripts/_0_5_7_1_rescore_platform_side.py
@@ -1,4 +1,19 @@
 from scorecard import Scorecard
+import boto3
+import os
+import json
+import io
+
+s3 = boto3.resource('s3')
+
+
+def load_json_file(file_name: str) -> dict:
+    with io.open(
+            file_name,
+            mode='r',
+            encoding='utf-8-sig') as json_file:
+        return json.loads(json_file.read())
+
 
 def rescore_platform_side(mongoDB):
     results_collection = mongoDB["eval_5_results"]
@@ -6,12 +21,22 @@ def rescore_platform_side(mongoDB):
 
     history_files = results_collection.find({"category": "interactive"})
 
-    for history in history_files:
-        scene = scenes_collection.find_one({"name": history["name"]})
-        scorecard = Scorecard(history, scene)
+    for history_record in history_files:
+        scene = scenes_collection.find_one({"name": history_record["name"]})
 
-        history["score"]["scorecard"]["correct_platform_side"] = scorecard.calc_correct_platform_side()
-        results_collection.replace_one({"_id": history["_id"]}, history)
+        #Download History file
+        bucket = s3.Bucket("evaluation-images")
+        history_file = "eval-resources-5/" + history_record["fullFilename"] + ".json"
+        basename = history_record["fullFilename"] + ".json"
+        print(f"Downloading {basename}")
+        bucket.download_file(history_file, basename)
+
+        history_item = load_json_file(basename)
+        scorecard = Scorecard(history_item, scene)
+
+        history_record["score"]["scorecard"]["correct_platform_side"] = scorecard.calc_correct_platform_side()
+        results_collection.replace_one({"_id": history_record["_id"]}, history_record)
+        os.remove(basename)
 
 
     print("Updated correct platform side fields.")
