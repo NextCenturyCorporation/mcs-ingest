ca4d25f6faf5e6c6890b0a3bdcf7ef53db0876a9
brianpippin <brianpippin@gmail.com>
2023-02-23 14:41:12 -0500

    Fix for some category types that were missed, made domain lower case to match mcs-private

--- scripts/_0_6_5_add_domain_type.py
+++ scripts/_0_6_5_add_domain_type.py
@@ -3,10 +3,16 @@
 #   the evaluation is run.
 #   This List was obtained doing a "distinct" on the dbs
 #   and then matched to list provided by psychs.
+
+# Some older evals had _ in between the words (hence while it appears
+#   to have duplicates)
 passive_objects = [
     "object_permanence",
 	"shape_constancy",
 	"spatio_temporal_continuity",
+    "object permanence",
+	"shape constancy",
+	"spatio temporal continuity",
     "gravity support",
     "collisions"
 ]
@@ -57,16 +63,16 @@ def add_domain_type_field(mongoDB):
         results_collection = mongoDB["eval_" + eval_number + "_results"]
 
         for item in passive_objects:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Passive Objects"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "passive objects"}}])
         for item in interactive_objects:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Interactive Objects"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "interactive objects"}}])
         for item in passive_agents:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Passive Agents"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "passive agents"}}])
         for item in interactive_agents:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Interactive Agents"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "interactive agents"}}])
         for item in interactive_places:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Interactive Places"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "interactive places"}}])
         for item in interactive:
-            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "Interactive"}}])
+            results_collection.update_many({"category_type": item}, [{"$set": {"domain_type": "interactive"}}])
 
         print("Updated Domain Types for Eval " + str(eval_number) + ".")
