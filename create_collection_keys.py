f0842830cb0b84c22078bc65e89c8ced2f87f310
Artiss <rachel.artiss@caci.com>
2023-05-24 16:22:59 -0400

    Only adding key for first step of steps array in eval_results collections for specific fields (MCS-1804).

--- create_collection_keys.py
+++ create_collection_keys.py
@@ -1,5 +1,17 @@
 import re
 
+# For the following step array fields, we want to save the 
+# value contained in the first step.
+STEPS_OUTPUT_FIELDS_TO_SAVE = [
+    "steps.output.position.x",
+    "steps.output.position.y",
+    "steps.output.position.z",
+    "steps.output.rotation",
+    "steps.output.target.position.x",
+    "steps.output.target.position.y",
+    "steps.output.target.position.z"
+]
+
 # Added check for '-' for dash because some scorecard fields have object ids, same for
 #  the numerical check.  None of our regular fields have '-' or numbers.
 def recursive_find_keys(x, keys, append_string):
@@ -14,11 +26,19 @@ def recursive_find_keys(x, keys, append_string):
                         arrayItem, keys, append_string + item + ".")
                 elif append_string + item not in keys and "-" not in (append_string + item) \
                          and not bool(re.search(r'\d', (append_string + item))):
-                    keys.append(append_string + item)
+                    append_key(append_string + item, keys)
+
         elif append_string + item not in keys and "-" not in (append_string + item) \
                 and not bool(re.search(r'\d', (append_string + item))):
-            keys.append(append_string + item)
+            append_key(append_string + item, keys)
 
+def append_key(key_string, keys):
+    if(key_string.startswith("steps.") is False):
+        keys.append(key_string)
+    elif key_string in STEPS_OUTPUT_FIELDS_TO_SAVE:
+        key_string = key_string[:6] + "0." + key_string[6:]
+        if(key_string not in keys):
+            keys.append(key_string)
 
 def find_collection_keys(index: str, collection_name: str, mongoDB):
     collection = mongoDB[index]
